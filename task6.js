9d2f74785a94303e38e0517be197cecc00044868
DanielleSaint <daniellesaint09@gmail.com>
2025-03-30 11:01:44 -0500

    Tareas 6 y 7 finalizadas

--- task6.js
+++ task6.js
@@ -6,19 +6,35 @@ export class members {
 }
 
 const member1 = new members("Dani",31);
-const member2 = new members("Fernando",31);
+const member2 = new members("Fernando",40);
+const member3 = new members("Kerly",25);
+const member4 = new members("Fabian",38); //agregar llamado
 
 export class team {
-    constructor(principiante) {
+    constructor() {
       this.principiante = []
-      console.log(this.principiante);
-      
+      this.eliminado = []
     }
     add(member){
-      this.principiante.push(member)
+        this.principiante.push(member)
+    }
+    remove(member){
+      for (let i = 0; i < this.principiante.length; i++) {
+        if (member === this.principiante[i]) {
+          this.eliminado.push(this.principiante[i])
+          this.principiante.splice(i,1);
+          return;
+        }
+      }
     }
 }
 
-const team1 = new team()
+const team1 = new team();
+team1.add(member1);
+team1.add(member2);
+team1.add(member3);
+team1.add(member4);
+team1.remove(member2);
+console.log(team1);
 
 
--- task7.js
+++ task7.js
@@ -1,7 +1,43 @@
-export class Player {
-    constructor() {
-      
-    }
-  
-    
+export class members {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+    this.items = []
+    this.itemEliminado = []
   }
+  add(item){
+    this.items.push(item);
+  }
+  remove(item){
+    for (let i = 0; i < this.items.length; i++) {
+      if (item === this.items[i]) {
+        this.itemEliminado.push(this.items[i])
+        this.items.splice(i,1);
+        return;
+      }
+    }
+  }
+}
+
+const member1 = new members("Dani",31);
+const member2 = new members("Fernando",40);
+const member3 = new members("Kerly",25);
+const member4 = new members("Fabian",38); 
+
+//& inventario
+member1.add("Baston")
+member1.add("Hongos")
+member2.add("Pocion")
+member2.add("Manzana")
+member3.add("Libro")
+member4.add("Katana")
+
+member1.remove("Baston")
+
+console.log(member1);
+console.log(member2);
+console.log(member3);
+console.log(member4);
+
+
+
